cb87e7edf804109fb965d5fb7cb27df0f26ee00b
ade <ade7758@163.com>
2018-11-26 18:30:37 +0800

    创建了login分支

---
+++ src/components/login/login.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<!--123456-->
+		<el-form :label-position="top" label-width="80px" :model="formLabelAlign">
+			<el-form-item label="名称">
+				<el-input v-model="formLabelAlign.name"></el-input>
+			</el-form-item>
+			<el-form-item label="活动区域">
+				<el-input v-model="formLabelAlign.region"></el-input>
+			</el-form-item>
+			<el-form-item label="活动形式">
+				<el-input v-model="formLabelAlign.type"></el-input>
+			</el-form-item>
+		</el-form>
+	</div>
+</template>
+
+<script>
+	export default {}
+</script>
+
+<style>
+
+</style>

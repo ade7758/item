4e21168e5fdc2b69abbf895e2705513db8db034e
ade <ade7758@163.com>
2018-11-27 17:29:19 +0800

    users模块

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,5 +1,6 @@
 <template>
 	<el-container class="container">
+		 <!--顶部-->
 		<el-header class="header">
 			<el-row>
 				<el-col :span="4">
@@ -13,19 +14,21 @@
 				</el-col>
 			</el-row>
 		</el-header>
+		
+		
 		<el-container>
+			 <!--侧边栏-->
 			<el-aside class="aside" width="200px">
 				<el-menu 
+					:router='true'
 					:unique-opened="true"
 					default-active="2" class="el-menu-vertical-demo" >
 					<el-submenu index="1">
 						<template slot="title">
 							<i class="el-icon-location"></i>
-							<span>导航一</span>
+							<span>用户管理</span>
 						</template>
-						<el-menu-item index="1-1">选项1</el-menu-item>
-						<el-menu-item index="1-1">选项1</el-menu-item>
-						<el-menu-item index="1-1">选项1</el-menu-item>
+						<el-menu-item index="users">用户列表</el-menu-item>
 					</el-submenu>
 					<el-submenu index="2">
 						<template slot="title">
@@ -65,32 +68,10 @@
 					</el-submenu>
 				</el-menu>
 			</el-aside>
+			
+			 <!--内容-->
 			<el-main class="main">
-				<el-table :data="formdata" style="width: 100%">
-					<el-table-column label="日期" width="180">
-						<template slot-scope="scope">
-							<i class="el-icon-time"></i>
-							<span style="margin-left: 10px">{{ formdata.data }}</span>
-						</template>
-					</el-table-column>
-					<el-table-column label="姓名" width="180">
-						<template slot-scope="scope">
-							<el-popover trigger="hover" placement="top">
-								<p>姓名: {{ formdata.name }}</p>
-								<p>住址: {{ formdata.data }}</p>
-								<div slot="reference" class="name-wrapper">
-									<el-tag size="medium">{{ scope.row.name }}</el-tag>
-								</div>
-							</el-popover>
-						</template>
-					</el-table-column>
-					<!--<el-table-column label="操作">
-						<template slot-scope="scope">
-							<el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-							<el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-						</template>
-					</el-table-column>-->
-				</el-table>
+				<router-view/>
 			</el-main>
 		</el-container>
 	</el-container>
@@ -110,16 +91,7 @@
 				this.$router.push({name:'login'})
 			}
 		},
-		data(){
-			return {
-				formdata :
-					[{data:100,
-					name:1,
-					}
-					]
-				
-			}
-		}
+		
 	}
 </script>
 
@@ -129,16 +101,16 @@
 	}
 	
 	.header {
-		background-color: #777;
+		background-color: floralwhite;
 	}
-	
+	/*
 	.aside {
 		background-color: #888;
 	}
 	
 	.main {
 		background-color: #999;
-	}
+	}*/
 	
 	.title {
 		line-height: 60px;
---
+++ src/components/users/users.vue
@@ -0,0 +1,45 @@
+<template>
+	<el-table :data="formdata" style="width: 100%">
+		<el-table-column label="日期" width="180">
+			<template slot-scope="scope">
+				<i class="el-icon-time"></i>
+				<span style="margin-left: 10px">{{ formdata.data }}</span>
+			</template>
+		</el-table-column>
+		<el-table-column label="姓名" width="180">
+			<template slot-scope="scope">
+				<el-popover trigger="hover" placement="top">
+					<p>姓名: {{ formdata.name }}</p>
+					<p>住址: {{ formdata.data }}</p>
+					<div slot="reference" class="name-wrapper">
+						<el-tag size="medium">{{ scope.row.name }}</el-tag>
+					</div>
+				</el-popover>
+			</template>
+		</el-table-column>
+		<!--<el-table-column label="操作">
+						<template slot-scope="scope">
+							<el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+							<el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+						</template>
+					</el-table-column>-->
+	</el-table>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				formdata: [{
+					data: 100,
+					name: 1,
+				}]
+
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
